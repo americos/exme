c78fe5e1f5fc8d87cc1f248a2fb6035c4b3b12e3
Americo Savinon <americos@gmail.com>
2011-01-12 12:58:39 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exme::Application.config.secret_token = '8f2012246e1c7a30aa68e239ce1bef0f91ebc1fa41a69800ee61f7ac8a089aa77fe0fc9ec1ca0576f4ef3069acdc545a7b327b490e06d2633d1bb9f4d7660df7'
